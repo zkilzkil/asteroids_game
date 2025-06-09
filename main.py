f5b71b2ccaec54175f9fd5d09cd071e3a070c101
zkilzkil <eskil.buvarp@gmail.com>
2025-06-09 20:05:04 +0200

    fix while loop, add close game window

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import pygame
 from constants import *
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
-	
-if __name__ == "__main__":
-	for event in pygame.event.get():
-    	if event.type == pygame.QUIT:
-        	return
-	run_program = True
-	while run_program:
+	while True:
 		screen.fill((0, 0, 0))
 		pygame.display.flip()
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return	
+
+if __name__ == "__main__":
+	
+	
+		
+
 	main()
