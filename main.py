244a145f815f0bd421fb84a3c96bc8dc00b11204
zkilzkil <eskil.buvarp@gmail.com>
2025-06-09 13:17:50 +0200

    fix pygame launcher

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+from constants import *
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def main():
+	print("Starting Asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
+	
+if __name__ == "__main__":
+	run_program = True
+	while run_program:
+		screen.fill((0, 0, 0))
+		pygame.display.flip()
+	main()
